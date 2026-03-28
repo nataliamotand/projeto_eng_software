4fd7f3fd39dd2961ef885db9bc2881b11ff0dc4c
nmota <nataliamotand@gmail.com>
2026-03-28 17:41:50 -0300

    adicao outros schemas

--- schemas.py
+++ schemas.py
@@ -1,13 +1,13 @@
 from pydantic import BaseModel
+from typing import List, Optional
 
-# 1. Esquema para CRIAR um usuário
+# --- ESQUEMAS PARA USUÁRIOS ---
 class UsuarioCreate(BaseModel):
     nome: str
     email: str
     senha: str
     tipo_perfil: str
 
-# 2. Esquema para DEVOLVER um usuário
 class UsuarioResponse(BaseModel):
     id: int
     nome: str
@@ -15,4 +15,49 @@ class UsuarioResponse(BaseModel):
     tipo_perfil: str
 
     class Config:
-        from_attributes = True 
+        from_attributes = True
+
+# --- ESQUEMAS PARA ALUNOS ---
+class AlunoCreate(BaseModel):
+    usuario_id: int
+    peso: float
+    altura: float
+    objetivo: str
+
+class AlunoResponse(BaseModel):
+    id: int
+    usuario_id: int
+    peso: float
+    altura: float
+    objetivo: str
+
+    class Config:
+        from_attributes = True
+
+# --- ESQUEMAS PARA EXERCÍCIOS ---
+class ExercicioCreate(BaseModel):
+    nome: str
+    grupo_muscular: str
+    series: int
+    repeticoes_base: int
+    carga_estimada: float
+
+class ExercicioResponse(ExercicioCreate):
+    id: int
+    ficha_id: int
+
+    class Config:
+        from_attributes = True
+
+# --- ESQUEMAS PARA FICHAS DE TREINO ---
+class FichaTreinoCreate(BaseModel):
+    aluno_id: int
+    titulo: str
+    status: Optional[str] = "ativa"
+
+class FichaTreinoResponse(FichaTreinoCreate):
+    id: int
+    exercicios: List[ExercicioResponse] = []
+
+    class Config:
+        from_attributes = True
