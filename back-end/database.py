6d3112336c28cc046db0f269d7f85cc46caf5ae7
Carlos Eduardo <c.eduardo021pro@gmail.com>
2026-04-11 19:58:41 -0300

    Adição de script de consulta de IDs e valida fluxo de fichas

--- back-end/database.py
+++ back-end/database.py
@@ -5,7 +5,11 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 SQLALCHEMY_DATABASE_URL = "postgresql://neondb_owner:npg_pHwCA26yWoxn@ep-soft-hill-acfe5uk3-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
 # O 'Engine' é o motor que gerencia a comunicação real com o banco de dados
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300
+)
 
 # A 'Session' é a nossa "janela" de conversa com o banco. 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
